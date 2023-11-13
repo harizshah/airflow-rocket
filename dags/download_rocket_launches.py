7a220bedc75d86d74d41828d99c407029456a684
Hariz Izzudin <harizsharizad@gmail.com>
2023-11-13 22:50:21 +0800

    project first time belajar

---
+++ dags/download_rocket_launches.py
@@ -0,0 +1,65 @@
+import json
+import pathlib
+
+import airflow.utils.dates
+import requests
+import requests.exceptions as requests_exceptions
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+#Instantiate a DAG object this is the starting point of any workflow.
+dag = DAG(
+    #name of DAG
+    dag_id="download_rocket_launches",
+    description="Download rocket pictures of recently launched rockets.",
+    #Date at which DAG should be running
+    start_date=airflow.utils.dates.days_ago(14),
+    schedule_interval="@daily",
+)
+
+#Download with curl with Task id
+download_launches = BashOperator(
+    task_id="download_launches",
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",  # noqa: E501
+    dag=dag,
+)
+
+def _get_pictures():
+    # Ensure directory exists
+    pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
+
+    # Download all pictures in launches.json
+    with open("/tmp/launches.json") as f:
+        launches = json.load(f)
+        image_urls = [launch["image"] for launch in launches["results"]]
+        for image_url in image_urls:
+            try:
+                response = requests.get(image_url)
+                image_filename = image_url.split("/")[-1]
+                target_file = f"/tmp/images/{image_filename}"
+                with open(target_file, "wb") as f:
+                    f.write(response.content)
+                print(f"Downloaded {image_url} to {target_file}")
+            except requests_exceptions.MissingSchema:
+                print(f"{image_url} appears to be an invalid URL.")
+            except requests_exceptions.ConnectionError:
+                print(f"Could not connect to {image_url}.")
+
+#Call the function in DAG with PythonOperator
+get_pictures = PythonOperator(
+    task_id="get_pictures", python_callable=_get_pictures, dag=dag
+)
+
+notify = BashOperator(
+    task_id="notify",
+    bash_command='echo "There are now $(ls /tmp/images/ | wc -l) images."',
+    dag=dag,
+)
+
+#Set the order of execution in task
+download_launches >> get_pictures >> notify
+
+
+
+
